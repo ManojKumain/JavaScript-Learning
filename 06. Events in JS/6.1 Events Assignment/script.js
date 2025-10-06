c25329a92cfecdfdc27815a2d7f20b8785615502
Manoj Chand <memanojkumain@gmail.com>
2025-10-07 03:29:23 +0530

    Completed assignment on events in JS

--- 06. Events in JS/6.1 Events Assignment/script.js
+++ 06. Events in JS/6.1 Events Assignment/script.js
@@ -2,9 +2,27 @@
 const btn = document.querySelector("#react-btn")
 const para = document.querySelector("#status");
 
+
+
 btn.addEventListener("click", function () {
     btn.textContent = "Clicked";
     document.body.style.backgroundColor = "green";
     para.textContent = "You clicked the button!"
+})
 
+btn.addEventListener("dblclick", function() {
+    btn.textContent = "Double Clicked";
+    document.body.style.backgroundColor = "red";
+    para.textContent = "You double-clicked the button!";
+})
+
+btn.addEventListener("mouseover", function() {
+    document.body.style.backgroundColor = "orange" ;
+    para.textContent = "Hovering...";
+})
+
+btn.addEventListener("mouseout", function(){
+    document.body.style.backgroundColor = 'blue';
+    para.textContent = "Mouse left the button"
+    btn.textContent = "Click Me!"
 })
